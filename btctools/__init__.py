7896e202b20381e71affe81279bd6294a7e33617
maxplanck013 <maxplanck013@gmail.com>
2020-09-16 21:45:14 -0300

    Add verbosity

--- btctools/__init__.py
+++ btctools/__init__.py
@@ -3,14 +3,24 @@ import base58
 import binascii
 
 
-def validate_btc_address(address):
+def validate_btc_address(address, verbose=False):
     decoded_address = base58.b58decode(address).hex()
     prefix = decoded_address[:-8]
     checksum = decoded_address[len(prefix):]
 
+    if verbose:
+        print(f"[info] decoded address = {decoded_address}")
+        print(f"[info] address prefix = {prefix}")
+        print(f"[info] address checksum = {checksum}")
+
     calc_checksum = prefix
+
+    if verbose: print("[info] calculating checksum...")
+
     for x in range(1,3):
         calc_checksum = binascii.unhexlify(calc_checksum)
         calc_checksum = hashlib.sha256(calc_checksum).hexdigest()
+
+    if verbose: print(f"[info] checksum = {calc_checksum[:8]}")
 
     return calc_checksum[:8] == checksum
--- validate_address.py
+++ validate_address.py
@@ -3,13 +3,19 @@ import btctools
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("btc_address", help="A bitcoin address.")
+verbosity = parser.add_mutually_exclusive_group()
+parser.add_argument("btc_address", type=str, help="a bitcoin address.")
+verbosity.add_argument("-q", "--quiet", action="store_true", default=False, help="don't output anything.")
+verbosity.add_argument("-v", "--verbose", action="store_true", default=False, help="show intermediate steps.")
 
-address = parser.parse_args().btc_address
+args = parser.parse_args()
 
-if btctools.validate_btc_address(address):
-    print(address)
+address = args.btc_address
+quiet = args.quiet
+
+if btctools.validate_btc_address(address, verbose=args.verbose):
+    if not quiet: print(address)
     exit(0)
 else:
-    print("Invalid address")
+    if not quiet: print("Invalid address")
     exit(1)
